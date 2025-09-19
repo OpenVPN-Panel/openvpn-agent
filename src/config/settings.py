ce8209c555021d22e4d07101e205cb627332b9f1
domster704 <domster704@mail.ru>
2025-09-19 20:04:07 +0300

    update Settings variables

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,16 +7,16 @@ ROOT_PATH = Path(sys.path[1])
 
 
 class Settings(BaseSettings):
-    mgmt_address: str
-    mgmt_port: int
-    scripts_dir: str
+    mgmt_address: str = "127.0.0.1"
+    mgmt_port: int = 55555
+    scripts_dir: str = "/root/openvpn/scripts"
 
-    model_config = SettingsConfigDict(
-        env_file=ROOT_PATH / ".env",
-        env_file_encoding="utf-8",
-        env_prefix="",
-        env_nested_delimiter="_",
-    )
+    # model_config = SettingsConfigDict(
+    #     env_file=ROOT_PATH / ".env",
+    #     env_file_encoding="utf-8",
+    #     env_prefix="",
+    #     env_nested_delimiter="_",
+    # )
 
 
 settings = Settings()
